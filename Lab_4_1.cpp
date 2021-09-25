d4cbcd2fe240621a400ba37d65b3d3760b6cfad0
Abhinav Pratap <abhinavpratap@Abhinavs-MacBook-Air.local>
2021-09-25 11:33:59 +0530

    Create Lab_4_1.cpp

---
+++ Lab_4_1.cpp
@@ -0,0 +1,10 @@
+//To implement C++ first program to print the statement "We are learning C++ Programming"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout<<"Abhinav Pratap"<<endl<<"A2345920104"<<endl<<"CSE Evening"<<endl;
+  cout<<"We are learning C++ Programming"<<endl;
+  return 0;
+}
